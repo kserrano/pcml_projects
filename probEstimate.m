f02862e64db7707ff58d9b6724d6f036f5768c4a
Youssef El Baba <youssef.baba@me.com>
2014-10-31 14:23:10 +0100

    changed ProbEstimate description

--- probEstimate.m
+++ probEstimate.m
@@ -1,7 +1,8 @@
-function [P] = probEstimate(tX, beta)
+function [P] = probEstimate(txBeta)
 % probEstimate compute the estimated probability from our inputs and beta
-% (JHWT book page 135, section 4.4)
+% (JHWT book page 135, section 4.4). The argument should be given as the
+% product of tx*beta, directly, for generecity reasons
 
-e = exp(tX'*beta);
+e = exp(tXBeta);
 P = e./(1+e);
 
