baf98df2a945ea405409b3c8efcaad2c3a05866f
Alexandre HELFRE <alexandre.helfre@epfl.ch>
2014-11-02 13:11:39 +0100

    ""

--- ridgeRegression.m
+++ ridgeRegression.m
@@ -1,5 +1,5 @@
 function beta = ridgeRegression(y,tX,lambda)
-% compute beta using the ridgeRegression
+% compute beta using the ridgeRegression 
 [~,M] = size(tX);
 beta = (tX'*tX + lambda*eye(M))\tX'*y;
 
